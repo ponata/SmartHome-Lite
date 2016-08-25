63b9b7562b085beedfdf9d871b6fea4a873b27ef
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-08-25 17:36:56 +0300

    fixed on/ff, validation inputs

--- pages/friedge/js/friedge.js
+++ pages/friedge/js/friedge.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
         inputFreezeTemp: document.getElementById("inputFreezeTemp"),
         inputOverallTemp: document.getElementById("inputOverallTemp"),
         inputsTemp: document.getElementsByClassName("input"),
-
         progressBar: document.getElementById("progressBar"),
         outputPercentage: document.getElementById("outputPercentage"),
         close: document.getElementById("close"),
@@ -12,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         outputModalMessage: document.getElementById("outputModalMessage"),
         defrost: document.getElementById("defrost"),
         svg: document.getElementById("svg"),
-        btnPower: document.getElementById("btnPower"),
         status: document.getElementById("status"),
         selectMode: document.getElementById("selectMode")
     }
@@ -27,52 +25,35 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     var modalMessages = {
-        frozen: "Frozen!",
-        defrozen: "Defrozen!"
-    }
-    htmlObjects.btnPower.onclick = function(e) {
+            frozen: "Frozen!",
+            defrozen: "Defrozen!"
+        }
+        // power button clicked
+    htmlObjects.status.onclick = function(e) {
 
         if (htmlObjects.status.checked) {
+            // friedge is on
+
             htmlObjects.svg.classList.add("on");
             htmlObjects.svg.classList.remove("off");
             startingValues.flag = "frost";
             Frost(htmlObjects, startingValues, modalMessages);
         } else {
+            // friedge is off
+
             htmlObjects.svg.classList.add("off");
             htmlObjects.svg.classList.remove("on");
-            startingValues.flag = "defrost";
-            Frost(htmlObjects, startingValues, modalMessages);
+            if (startingValues.flag == "frost") {
+                startingValues.flag = "defrost";
+                Frost(htmlObjects, startingValues, modalMessages);
+
+            } else {}
+
         }
 
     }
 
-    htmlObjects.selectMode.onchange = function() {
 
-        switch (htmlObjects.selectMode.options[htmlObjects.selectMode.selectedIndex].value) {
-            case "standard":
-                htmlObjects.inputFreezeTemp.value = "-18";
-                htmlObjects.inputOverallTemp.value = "-6";
-                htmlObjects.svg.classList.add("standard");
-                htmlObjects.svg.classList.remove("light");
-                htmlObjects.svg.classList.remove("highFreeze");
-                break;
-            case "light":
-                htmlObjects.inputFreezeTemp.value = "-9";
-                htmlObjects.inputOverallTemp.value = "-3";
-                htmlObjects.svg.classList.add("light");
-                htmlObjects.svg.classList.remove("standard");
-                htmlObjects.svg.classList.remove("highFreeze");
-                break;
-            case "highFreeze":
-                htmlObjects.inputFreezeTemp.value = "-27";
-                htmlObjects.inputOverallTemp.value = "-10";
-                htmlObjects.svg.classList.add("highFreeze");
-                htmlObjects.svg.classList.remove("standard");
-                htmlObjects.svg.classList.remove("light");
-                break;
-        }
-
-    }
 
 
 });
@@ -83,74 +64,99 @@ function Frost(htmlObjects, startingValues, modalMessages) {
 
     switch (startingValues.flag) {
 
-        // Start to froze
+        // Start frost
         case "frost":
 
             startingValues.speed = 70;
             startingValues.currentTemps = getCurrentTemps(htmlObjects);
 
             var timeoutID = setInterval(function() {
-            	if (true) {}
-                htmlObjects.inputOverallTemp.onchange = function() {
-                	console.log('new');
-                    startingValues.currentTemps = getCurrentTemps(htmlObjects);
+                // before new iteration, check power button status
+                if (htmlObjects.status.checked) {
+                    // check input, if it was changed
+                    document.getElementById('inputWrapper').addEventListener('change', function(event) {
+                        var elem = event.target;
+                        if (checkInput(elem)) {
+                            startingValues.currentTemps = getCurrentTemps(htmlObjects);
+                        }
+                    });
+                    // check if mode was selected
+                    htmlObjects.selectMode.onchange = function() {
+                            selectMode(htmlObjects);
+                            startingValues.currentTemps = getCurrentTemps(htmlObjects);
+                        }
+                        // calc the step
+                    startingValues.stepForward = (startingValues.currentTemps.inputFreezeTemp + startingValues.currentTemps.inputOverallTemp) / 200;
 
-                }
+                    if (startingValues.currentWidth + startingValues.stepForward < 100) {
+                        startingValues.currentWidth += startingValues.stepForward;
 
-                startingValues.stepForward = (startingValues.currentTemps.inputFreezeTemp + startingValues.currentTemps.inputOverallTemp) / 5000;
-                if (startingValues.currentWidth + startingValues.stepForward < 100) {
-                    startingValues.currentWidth += startingValues.stepForward;
+                        htmlObjects.outputPercentage.innerHTML = parseInt(startingValues.currentWidth) + '%';
+                        htmlObjects.progressBar.style.width = startingValues.currentWidth + '%';
 
-                    htmlObjects.outputPercentage.innerHTML = parseInt(startingValues.currentWidth) + '%';
-                    htmlObjects.progressBar.style.width = startingValues.currentWidth + '%';
+                    } else {
+                        startingValues.currentWidth = 100;
+                        htmlObjects.outputPercentage.innerHTML = '100%';
+                        htmlObjects.progressBar.style.width = '100%';
 
+                        showModal(htmlObjects, startingValues, modalMessages);
+
+                        clearTimeout(timeoutID);
+                        setTimeout(function() {
+                            // run Frost with new flag
+                            startingValues.flag = "defrost";
+                            Frost(htmlObjects, startingValues, modalMessages);
+                        }, 3000);
+                    }
                 } else {
-                    startingValues.currentWidth = 100;
-                    htmlObjects.outputPercentage.innerHTML = '100%';
-                    htmlObjects.progressBar.style.width = '100%';
-
-                    showModal(htmlObjects, startingValues, modalMessages);
-
+                    // if the friedge turned off
                     clearTimeout(timeoutID);
-                    setTimeout(function() {
-                        startingValues.flag = "defrost";
-                        Frost(htmlObjects, startingValues, modalMessages);
-                    }, 3000);
+                    return false;
                 }
+
             }, startingValues.speed);
 
             break;
 
-
-            // start to derfrost
+            // start derfrost
         case "defrost":
 
             startingValues.speed = 40;
 
             var timeoutID = setInterval(function() {
+                // before new iteration, check the status of the friedge
+                if (true) {
+                    if (startingValues.currentWidth - startingValues.stepRevert > 0) {
+                        startingValues.currentWidth -= startingValues.stepRevert;
+                        htmlObjects.outputPercentage.innerHTML = parseInt(startingValues.currentWidth) + '%';
+                        htmlObjects.progressBar.style.width = startingValues.currentWidth + '%';
+                    } else {
+                        startingValues.currentWidth = 0;
+                        htmlObjects.outputPercentage.innerHTML = '0%';
+                        htmlObjects.progressBar.style.width = '0%';
+                        showModal(htmlObjects, startingValues, modalMessages);
 
-                if (startingValues.currentWidth - startingValues.stepRevert > 0) {
-                    startingValues.currentWidth -= startingValues.stepRevert;
-                    htmlObjects.outputPercentage.innerHTML = parseInt(startingValues.currentWidth) + '%';
-                    htmlObjects.progressBar.style.width = startingValues.currentWidth + '%';
+                        clearTimeout(timeoutID);
+
+                        if (htmlObjects.status.checked) {
+
+                            startingValues.flag = "frost";
+                            Frost(htmlObjects, startingValues, modalMessages);
+                        } else {
+
+                            clearTimeout(timeoutID);
+                            return false;
+                        }
+                    }
                 } else {
-                    startingValues.currentWidth = 0;
-                    htmlObjects.outputPercentage.innerHTML = '0%';
-                    htmlObjects.progressBar.style.width = '0%';
-                    showModal(htmlObjects, startingValues, modalMessages);
 
-                    clearTimeout(timeoutID);
-                    startingValues.flag = "frost";
-                    if (htmlObjects.status.checked) {
-                        Frost(htmlObjects, startingValues, modalMessages);
-                    } else {}
 
                 }
+
 
             }, startingValues.speed);
 
             break;
-
     }
 }
 
@@ -162,8 +168,6 @@ function getCurrentTemps(htmlObjects) {
         inputOverallTemp: Math.abs(htmlObjects.inputOverallTemp.value)
     }
     return object;
-
-
 }
 
 function showModal(htmlObjects, startingValues, modalMessages) {
@@ -196,6 +200,65 @@ function showModal(htmlObjects, startingValues, modalMessages) {
         if (e.target == htmlObjects.modal) {
             htmlObjects.modal.style.display = "none";
         } else {}
+
+    }
+
+}
+
+function checkInput(elem) {
+    if (elem.id == "inputOverallTemp") {
+        if (elem.value >= -20 && elem.value <= 0) {
+            return true
+        } else {
+            return false;
+        }
+
+    }
+    if (elem.id == "inputFreezeTemp") {
+        if (elem.value >= -50 && elem.value <= 0) {
+            return true
+        } else {
+            return false;
+        }
+
+    }
+}
+
+function selectMode(htmlObjects) {
+
+    switch (htmlObjects.selectMode.options[htmlObjects.selectMode.selectedIndex].value) {
+        case "standard":
+            htmlObjects.inputFreezeTemp.value = "-18";
+            htmlObjects.inputOverallTemp.value = "-6";
+            // htmlObjects.svg.classList.add("standard");
+            // htmlObjects.svg.classList.remove("light");
+            // htmlObjects.svg.classList.remove("highFreeze");
+            break;
+        case "light":
+            htmlObjects.inputFreezeTemp.value = "-9";
+            htmlObjects.inputOverallTemp.value = "-3";
+            // htmlObjects.svg.classList.add("light");
+            // htmlObjects.svg.classList.remove("standard");
+            // htmlObjects.svg.classList.remove("highFreeze");
+            break;
+        case "highFreeze":
+            htmlObjects.inputFreezeTemp.value = "-27";
+            htmlObjects.inputOverallTemp.value = "-10";
+            // htmlObjects.svg.classList.add("highFreeze");
+            // htmlObjects.svg.classList.remove("standard");
+            // htmlObjects.svg.classList.remove("light");
+            break;
+    }
+
+}
+
+function setColor(htmlObjects, startingValues) {
+    startingValues.currentTemps = getCurrentTemps(htmlObjects);
+    switch (startingValues.currentTemps.inputFreezeTemp + startingValues.currentTemps.inputOverallTemp) {
+        case expression:
+
+            break;
+        default:
 
     }
 
