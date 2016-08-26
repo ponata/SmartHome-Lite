3c877bc3da210dceb7c6548358585d9a9f29d021
Anton Myshenin <myshenin.social@gmail.com>
2016-08-26 22:03:41 +0300

    bug fixed
    smoth transition bug fixed

--- scripts/device-air-conditioner.js
+++ scripts/device-air-conditioner.js
@@ -12,7 +12,7 @@ window.onload = function() {
     function switchMode(start, end){
         var interval = start;
         var stop = setInterval(function() {
-            interval+=((start < end? 1: -1)*10);
+            interval+=((start < end? 1: -1)*1);
             clearInterval(spin);
             spin = setInterval(rotate, interval);
             
@@ -25,18 +25,20 @@ window.onload = function() {
         }, 500);
     }
     //Events
+    var speed = document.getElementById("speed-input");
     document.getElementById("on").onclick = function() {
-        switchMode(40, 40 - 10 * (document.getElementById("speed-input").value - 1));
+        switchMode(40, 40 - 10 * (speed.value - 1));
     }
     document.getElementById("off").onclick = function() {
-        switchMode(40 - 10 * (document.getElementById("speed-input").value - 1), 40);
+        switchMode(40 - 10 * (speed.value - 1), 40);
     }
+    
+    var prevSpeed = speed.value; 
     var off = document.getElementById("off");
     document.getElementById("speed-input").onchange = function() {
         if(!off.checked) {
-            clearInterval(spin);
-            spin = setInterval(rotate, 40 - 10 * (document.getElementById("speed-input").value - 1));
-            console.log(40 - 10 * (document.getElementById("speed-input").value - 1));
+            switchMode(prevSpeed, speed.value);
+            prevSpeed = speed.value;
         }
     }
 }
